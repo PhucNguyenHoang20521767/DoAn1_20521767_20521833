ba80a9e943cc72914df488a65f02391f94cbb804
Sang <75106540+sang1833@users.noreply.github.com>
2023-07-10 11:44:10 +0700

    order detail

---
+++ src/components/BillAddress.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { getAllOrder, getOrderItemByOrder, getAddressById, getProductById } from '@/api/api_function'
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store/store';
+
+interface Props {
+    addressId: string;
+}
+
+const BillAddress = ({addressId}: any) => {
+    const currentUser = useSelector((state: RootState) => state.auth.currentUser)
+    const [address, setAddress] = React.useState<any| null>(null);
+
+    React.useEffect(() => {
+        getAddressById(addressId, currentUser).then((res) => {
+            const addressRes = res.data.data
+            // console.log("address", addressRes)
+            setAddress(addressRes)
+        })
+    }, [addressId])
+
+  return (
+    <>
+        {address && (
+        <div key={address._id} className='border-2 mt-3 w-full pb-2'>
+          <div className='flex justify-between'>
+            <div className='flex flex-col'>
+              <div className='flex flex-row'>
+                <div className='text-primary-1 p-2'>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <p className='font-bold py-2'>{address.receiverFirstName} {address.receiverLastName}</p>
+              </div>
+              <div className='flex flex-row'>
+                <div className='text-primary-1 p-2'>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <p className='py-2'>{address.receiverPhone}</p>
+              </div>
+              <div className='flex flex-row'>
+                <div className='text-primary-1 p-2'>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <p className='py-2 text-gray-500'>{address.receiverAddress}, {address.receiverWard}, {address.receiverDistrict}, {address.receiverCity}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        )
+        }
+    </>
+  )
+}
+
+export default BillAddress
---
+++ src/components/BillProductList.tsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from 'react';
+import { RootState } from '@/redux/store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation, useNavigate, useLoaderData, useParams } from 'react-router-dom';
+import {
+  getProductById,
+  getProductColor,
+  getProductColorById,
+  getAllProductImageUrlByColor,
+  getProductImagesUrl,
+  getDiscountById,
+  getOrderItemByOrder,
+} from '@/api/api_function';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { set } from 'react-hook-form';
+
+interface OrderItem {
+  _id: string;
+  orderId: string;
+  productId: string;
+  productColorId: string;
+  productQuantity: number;
+  productPrice: number;
+  productSalePrice: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface IOrderState {
+    _id: string;
+    customerId: string;
+    staffId: string | null;
+    orderCode: string;
+    orderStatus: string;
+    orderNote: string;
+    orderAddress: string;
+    paymentMethod: string;
+    orderShippingFee: number;
+    createdAt: string;
+    updatedAt: string;
+  }
+
+interface BillProductListProps {
+  orderItem: OrderItem;
+  orderInfo: IOrderState | undefined;
+}
+
+const BillProductList = ({ orderItem, orderInfo }: BillProductListProps) => {
+  const navigate = useNavigate();
+//   const orderInfo = useSelector((state: RootState) => state.order.order);
+  const [product, setProduct] = useState<any>(null);
+  const [color, setProductColor] = useState<any>(null);
+  const [productImageUrl, setProductImageUrl] = useState<string[] | null>(null);
+  const [canReview, setCanReview] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (orderInfo?.orderStatus === 'Đã hoàn tất') {
+            setCanReview(true);
+        }
+        else {
+            setCanReview(false);
+        }
+    }, [orderInfo]);
+
+  useEffect(() => {
+    getProductById(orderItem.productId).then((res) => {
+      const productRes = res.data.data;
+      console.log('productRes', productRes);
+      setProduct(productRes);
+    });
+
+    getProductColorById(orderItem.productColorId).then((res) => {
+      const productColorRes = res.data.color;
+      console.log('productColorRes', productColorRes);
+      setProductColor(productColorRes);
+    });
+
+    getAllProductImageUrlByColor(orderItem.productId, orderItem.productColorId).then((res) => {
+      const productImageUrlRes = res.data.data;
+      console.log('productImageUrlRes', productImageUrlRes);
+      const _imageUrls = productImageUrlRes.map((productImageUrl: any) => productImageUrl.imageURL);
+      setProductImageUrl(_imageUrls);
+    });
+  }, [orderItem]);
+
+  return (
+    <>
+        {
+            orderItem && product && color && productImageUrl && (
+            <div className='max-w-3xl rounded overflow-hidden shadow m-2 px-4'>
+            <div className="flex justify-between p-4">
+                <div className="flex gap-4 items-center">
+                {productImageUrl && productImageUrl.length > 0 && (
+                    <img
+                    key={orderItem.productColorId}
+                    src={productImageUrl[0]}
+                    alt={product?.productName}
+                    className="w-16 h-16 object-contain"
+                    />
+                )}
+                <button className="text-dark-3" onClick={() => navigate(`/collection/${orderItem.productId}`)}>
+                    Xem
+                </button>
+                <div>
+                    <div className="font-bold">{product?.productName}</div>
+                    <div className="flex">
+                    <div className="font-bold">Màu: </div>
+                    <div className="ml-1">{color?.colorName}</div>
+                    <div className="flex items-center ml-2">
+                        <span className={`inline-block w-4 h-4 rounded-full mr-2 bg-${color?.colorHex}`} style={{ backgroundColor: color?.colorHex }} />
+                    </div>
+                    </div>
+                    <div className="flex">
+                    <div className="font-bold">Số lượng: </div>
+                    <div className="ml-1">{orderItem.productQuantity}</div>
+                    </div>
+                    <div>{orderItem?.productSalePrice.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</div>
+                </div>
+                </div>
+                <div>
+                    {
+                        canReview && (
+                            <Link to={`/review/${orderItem.productId}/${orderItem.productColorId}`}>
+                                <button className="bg-dark-2 hover:bg-black text-white font-bold py-2 px-4 rounded">
+                                    Đánh giá
+                                </button>
+                            </Link>
+                        )
+                    }
+                </div>
+            </div>
+            </div>
+            )
+        }
+    </>
+  );
+};
+
+export default BillProductList;
